9330b2db17e40a64d1b1b7afd9f993b42b3665e5
thonguyenp <nphuoctho2406@gmail.com>
2025-02-14 17:59:07 +0700

    Hoàn thành bài 1 của lab 2

---
+++ BaiLab2/src/BaiLab2/Bai1.java
@@ -0,0 +1,28 @@
+package BaiLab2;
+
+import java.util.Scanner;
+
+public class Bai1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập vào số a :");
+		double a = sc.nextDouble();
+		System.out.print("Nhập vào số b :");
+		double b = sc.nextDouble();
+		
+		if (a == 0)
+		{
+			if (b == 0)
+			{
+				System.out.println("Phương trình vô số nghiệm");
+			}
+			else
+				System.out.println("Phương trình vô nghiệm");
+		}
+		else
+			System.out.println("Nghiệm pt: " + (-b)/a);
+	}
+
+}
---
+++ BaiLab2/src/BaiLab2/Bai2.java
@@ -0,0 +1,10 @@
+package BaiLab2;
+
+public class Bai2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

71de592bcc6bf51e66d4242c4eb9f67a539698fb
thonguyenp <nphuoctho2406@gmail.com>
2025-02-14 18:13:45 +0700

    Đã hoàn thành bài 3 lab 2

---
+++ BaiLab2/src/BaiLab2/Bai3.java
@@ -0,0 +1,25 @@
+package BaiLab2;
+
+import java.util.Scanner;
+
+public class Bai3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập vào số điện đã sử dụng: ");
+		int soDien = sc.nextInt();
+		
+		if (soDien <= 50 && soDien >= 0)
+		{
+			int soTien = soDien * 1000;
+			System.out.print("Số tiền phải trả: "+ soTien);
+		}	
+		else if (soDien > 50)
+		{
+			int soTien = 50 * 1000 + (soDien-50) * 1200;
+			System.out.print("Số tiền phải trả: "+ soTien);
+		}
+	}
+
+}

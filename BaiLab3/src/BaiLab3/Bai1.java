dfb32d4355baa5c4ad751fedef97a5eda50ae57d
thonguyenp <nphuoctho2406@gmail.com>
2025-02-16 19:45:28 +0700

    Hoàn thành bài 2 và chỉnh sửa bài 1 lab 3

--- BaiLab3/src/BaiLab3/Bai1.java
+++ BaiLab3/src/BaiLab3/Bai1.java
@@ -7,13 +7,14 @@ public class Bai1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập vào 1 số >=0: ");
 		int n = sc.nextInt();
 		for (int i = 2; i * i <= n; i++)
 		{
 			if (n % i == 0) 
 				System.out.printf("Số %d không phải là SNT", n);
 		}
-		System.out.printf("Số %d phải là SNT", n);
+		System.out.printf("Số %d là SNT", n);
 	}
 
 }
---
+++ BaiLab3/src/BaiLab3/Bai2.java
@@ -0,0 +1,18 @@
+package BaiLab3;
+
+public class Bai2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 9; i++)
+		{
+			for (int j = 1; j <= 10; j++)
+			{
+				System.out.printf("|%d x %d = %d|",j,i,i*j);
+			}
+			System.out.println();
+		}
+				
+	}
+
+}

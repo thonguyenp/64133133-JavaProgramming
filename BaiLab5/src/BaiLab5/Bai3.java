fe555811feb49ae0884ce8c5ef219b7caf9b259c
thonguyenp <nphuoctho2406@gmail.com>
2025-02-18 19:17:40 +0700

    Hoàn thành thao tác nhập, sắp xếp và xuất của bài 3 lab 5

---
+++ BaiLab5/src/BaiLab5/Bai3.java
@@ -0,0 +1,109 @@
+package BaiLab5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Bai3 {
+	private static ArrayList<Product> productList = new ArrayList<Product>();
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void nhap ()
+	{
+		System.out.print("Nhập vào số lượng sản phẩm");
+		int n = sc.nextInt();
+		sc.nextLine();
+		for (int i = 0; i < n; i++)
+		{
+			System.out.printf("Nhập tên sản phẩm thứ %d: ",i);
+			String name = sc.nextLine();
+			System.out.printf("Nhập giá sản phẩm thứ %d: ",i);
+			double price = sc.nextDouble();
+			sc.nextLine();
+			productList.add(new Product(name, price));
+		}
+	}
+	public static void sapXepTheoGia ()
+	{
+		Comparator<Product> comp = new Comparator<Product>() {
+			
+			@Override
+			public int compare(Product o1, Product o2) {
+				// TODO Auto-generated method stub
+                return Double.compare(o2.price, o1.price);
+			}
+		};
+		Collections.sort(productList,comp);	
+		System.out.println("Danh sách sản phẩm giảm theo giá:");	
+	}
+	
+	public static void xuat()
+	{
+		for (Product p : productList)
+			System.out.println(p);
+	}
+	
+	public static void menu ()
+	{
+		int choice;
+		do
+		{
+			System.out.println("\nMenu:");
+            System.out.println("1. Nhập danh sách sản phẩm");
+            System.out.println("2. Sắp xếp giảm dần theo giá");
+            System.out.println("3. Tìm và xóa tên nhập từ bàn phím:");
+            System.out.println("4. Xuất giá trung bình:");
+            System.out.println("5. Thoát chương trình:");
+    		choice = sc.nextInt();
+            sc.nextLine(); 
+            switch (choice) {
+            case 1:
+                nhap();
+                break;
+            case 2:
+                sapXepTheoGia();
+                xuat();
+                break;
+//            case 3:
+//                ngaunhien();
+//                break;
+//            case 4:
+//            	sapXep();
+//            	break;
+            case 5:
+                System.out.println("Chương trình kết thúc.");
+                break;
+            default:
+                System.out.println("Lựa chọn không hợp lệ, vui lòng chọn lại!");
+
+            }
+
+		}
+		while (choice != 5);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		menu();
+	}
+
+}
+	
+class Product
+{
+	String name;
+	double price;
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + "]";
+	}
+	
+	
+}

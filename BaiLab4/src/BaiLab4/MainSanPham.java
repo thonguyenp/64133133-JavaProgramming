92545ab7dc42995410ee52dd4df9dd1398191413
thonguyenp <nphuoctho2406@gmail.com>
2025-02-18 07:02:37 +0700

    Hoàn thành bài 2 lab 4 ở class MainSanPham

--- BaiLab4/src/BaiLab4/MainSanPham.java
+++ BaiLab4/src/BaiLab4/MainSanPham.java
@@ -4,9 +4,13 @@ public class MainSanPham {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SanPham sp1 = new SanPham();		
+		SanPham sp1 = new SanPham();	
+		SanPham sp2 = new SanPham();	
+
 		sp1.nhap();
+		sp2.nhap();
 		sp1.xuat();
+		sp2.xuat();
 		System.out.println(sp1.getThueNhapKhau());
 
 	}

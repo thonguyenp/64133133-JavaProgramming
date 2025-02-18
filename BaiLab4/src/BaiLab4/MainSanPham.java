695e44b88ead4187a4c42b58dcbc9fccba198526
thonguyenp <nphuoctho2406@gmail.com>
2025-02-18 10:22:32 +0700

    Hoàn thành bài 3 và 4 của lab 4

--- BaiLab4/src/BaiLab4/MainSanPham.java
+++ BaiLab4/src/BaiLab4/MainSanPham.java
@@ -4,15 +4,16 @@ public class MainSanPham {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SanPham sp1 = new SanPham();	
-		SanPham sp2 = new SanPham();	
+		SanPham sp1 = new SanPham("San pham 1", 500, 20);	
+		SanPham sp2 = new SanPham("San pham 2", 910.2, 10);	
+		SanPham sp3 = new SanPham("San pham 3", 800);	
 
-		sp1.nhap();
-		sp2.nhap();
-		sp1.xuat();
-		sp2.xuat();
-		System.out.println(sp1.getThueNhapKhau());
-
+//		sp1.nhap();
+//		sp2.nhap();
+//		sp1.xuat();
+//		sp2.xuat();
+//		System.out.println(sp1.getThueNhapKhau());
+		sp3.xuat();
 	}
 
 }
--- BaiLab4/src/BaiLab4/SanPham.java
+++ BaiLab4/src/BaiLab4/SanPham.java
@@ -5,10 +5,41 @@ import java.util.Scanner;
 public class SanPham {
 	private String tenSp;
 	private double donGia, giamGia;	
-	public SanPham()
-	{
-		
+	
+	public SanPham(String tenSp, double donGia, double giamGia) {
+		this.tenSp = tenSp;
+		this.donGia = donGia;
+		this.giamGia = giamGia;
 	}
+
+	public SanPham(String tenSp, double donGia) {
+		this(tenSp,donGia,0);
+	}
+
+	public String getTenSp() {
+		return tenSp;
+	}
+
+	public void setTenSp(String tenSp) {
+		this.tenSp = tenSp;
+	}
+
+	public double getDonGia() {
+		return donGia;
+	}
+
+	public void setDonGia(double donGia) {
+		this.donGia = donGia;
+	}
+
+	public double getGiamGia() {
+		return giamGia;
+	}
+
+	public void setGiamGia(double giamGia) {
+		this.giamGia = giamGia;
+	}
+
 	public void nhap() 
 	{
 		Scanner sc = new Scanner(System.in);
@@ -25,7 +56,7 @@ public class SanPham {
 		System.out.println( "SanPham [tenSp=" + tenSp + ", donGia=" + donGia + ", giamGia=" + giamGia + ", ThueNhapKhau=" + getThueNhapKhau()+ "]");
 	}
 	
-	public double getThueNhapKhau()
+	private double getThueNhapKhau()
 	{
 		return 0.1 * donGia;
 	}

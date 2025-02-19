4704b470338a5429fb2454bf57da387804e45a18
thonguyenp <nphuoctho2406@gmail.com>
2025-02-19 21:04:16 +0700

    Hoàn thành ví dụ nhập xuất

---
+++ VDNhapXuat/src/CTTinhTong/CTTinhTong.java
@@ -0,0 +1,20 @@
+package CTTinhTong;
+
+import java.util.Scanner;
+
+public class CTTinhTong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Nhập vào số a = ");
+		double a = sc.nextDouble();
+		System.out.print("Nhập vào số b = ");
+		double b = sc.nextDouble();
+		double sum = a + b;
+		System.out.print("Tổng là: "+ sum);
+
+	}
+
+}

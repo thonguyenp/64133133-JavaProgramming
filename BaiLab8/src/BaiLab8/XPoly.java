98521a0839a3147091cb6a069319bed3fffbeb14
thonguyenp <nphuoctho2406@gmail.com>
2025-02-23 14:56:01 +0700

    Hoàn thành bài 3 lab 8

--- BaiLab8/src/BaiLab8/XPoly.java
+++ BaiLab8/src/BaiLab8/XPoly.java
@@ -33,15 +33,31 @@ public class XPoly {
 		return max;
 	}
 
+	public static String toUpperFirstChar (String s)
+	{
+		String[] words = s.trim().split(" ");
+		for (int i = 0; i < words.length; i++)
+		{
+			if (!words[i].isEmpty())
+			{
+				char firstChar = words[i].charAt(0);
+				String upperFirst = String.valueOf(firstChar).toUpperCase();
+                words[i] = upperFirst + words[i].substring(1);
+
+			}
+		}
+		return String.join(" ", words);
+	}
+	
 	public static void main(String[] args) {
 		double resSum = sum(10,20,40);
 		double resMin = min(10,20,40);
 		double resMax = max(10,20,40);
+		String ten = toUpperFirstChar("nguyen tho");
 		
 		System.out.println("Tổng: " + resSum);
 		System.out.println("Nhỏ nhất: " + resMin);
 		System.out.println("Lớn nhất: " + resMax);
-
-
+		System.out.println("Tên: "+ ten);
 	}
 }
